551aa4d27ed6641208801f75a2ef219defe98a85
Andrey Evdokimov <andrey.evdokimov@mail.ru>
2018-09-25 23:33:37 +0300

    WIP

--- watcher.js
+++ watcher.js
@@ -5,6 +5,8 @@ const path = require('path');
 const cache = {};
 
 function send(params) {
+  const username = params.apiAccountName;
+  const password = params.apiAccountPassword;
   const filePath = params.filePath;
   const options = {
     hostname: params.apiServerName,
@@ -13,36 +15,32 @@ function send(params) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': Buffer.from(`${params.apiAccountName}:${params.apiAccountPassword}`).toString('base64')
-    }
+      Authorization: Buffer.from(`${username}:${password}`).toString('base64'),
+    },
   };
   const body = {
     Name: params.libName,
     Package: params.libPackage,
-    Script: params.libContent
+    Script: params.libContent,
   };
+  let hash;
 
   switch (params.event) {
     case 'create':
     case 'update':
-      const hash = crypto.createHash('md5').update(params.libContent).digest('hex');
+      hash = crypto.createHash('md5').update(params.libContent).digest('hex');
 
       if (cache[filePath] !== hash) {
         cache[filePath] = hash;
       }
       break;
     case 'delete':
-      
-      break;  
+
+      break;
     default:
       break;
   }
 
-
-
-  
-
-  
   /*
   const req = http.request(options, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
@@ -59,7 +57,7 @@ function send(params) {
   req.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
   });
-  
+
   req.write(postData);
   req.end();
   */
@@ -75,14 +73,16 @@ function getConfig() {
     apiAccountName: 'falcon',
     apiAccountPassword: '',
     libPackage: 'Custom',
-    validExtensions: ['.js']
+    validExtensions: ['.js'],
   };
 
   try {
     fs.statSync(configPath);
     result = JSON.parse(fs.readFileSync(configPath, { encoding: 'utf8' }));
-  } catch(err) {
-    if (err && err.code === 'ENOENT') { fs.writeFileSync(configPath, JSON.stringify(result, null, 2)); }
+  } catch (err) {
+    if (err && err.code === 'ENOENT') {
+      fs.writeFileSync(configPath, JSON.stringify(result, null, 2));
+    }
   }
 
   return result;
@@ -91,15 +91,16 @@ function getConfig() {
 function main() {
   const config = getConfig();
   const srcPath = config.srcPath;
+  const validExtensions = config.validExtensions;
 
   fs.watch(srcPath, { recursive: true }, (eventType, filename) => {
     const filePath = path.join(srcPath, filename);
-    const parts = path.parse(filePath);
-    const params = Object.assign({}, config, { libName: parts.name, libContent: '', filePath: filePath });
-    
-    if (validExtensions.includes(parts.ext)) {
-      fs.stat(filePath, (err, stats) => {
-        if (err && err.code === 'ENOENT') {
+    const pathParts = path.parse(filePath);
+    const params = Object.assign({}, config, { libName: pathParts.name, libContent: '', filePath });
+
+    if (validExtensions.includes(pathParts.ext)) {
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr && statErr.code === 'ENOENT') {
           params.event = 'delete';
           send(params);
         }
@@ -109,8 +110,8 @@ function main() {
             send(params);
           } else {
             params.event = 'update';
-            fs.readFile(filePath, (err, data) => {
-              if (err) { throw err; }
+            fs.readFile(filePath, (readErr, data) => {
+              if (readErr) { throw readErr; }
               params.content = data.toString('utf8');
               send(params);
             });
@@ -121,4 +122,4 @@ function main() {
   });
 }
 
-console.log(path.join(__dirname, 'src'));
+main();
